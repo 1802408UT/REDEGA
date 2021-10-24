56b11fa7af3c11c02a29c308545c124ff7dd5b6c
1802808UT <1802408@utrivieramaya.edu.mx>
2021-10-24 14:36:29 -0500

    add Services of Firebase
    Implementación para el despliegue de la aplicación con Firebase Hosting.

--- REDEGA/src/environments/environment.prod.ts
+++ REDEGA/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'redega-9c018',
+    appId: '1:698663162465:web:3a0e9cdfae8c8fdf9dd020',
+    storageBucket: 'redega-9c018.appspot.com',
+    apiKey: 'AIzaSyAUYJU6waclzySa50w34c-qkWOzr8W5pU4',
+    authDomain: 'redega-9c018.firebaseapp.com',
+    messagingSenderId: '698663162465',
+    measurementId: 'G-RGJ1F6KQ8H',
+  },
   production: true
 };
--- REDEGA/src/environments/environment.ts
+++ REDEGA/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'redega-9c018',
+    appId: '1:698663162465:web:3a0e9cdfae8c8fdf9dd020',
+    storageBucket: 'redega-9c018.appspot.com',
+    apiKey: 'AIzaSyAUYJU6waclzySa50w34c-qkWOzr8W5pU4',
+    authDomain: 'redega-9c018.firebaseapp.com',
+    messagingSenderId: '698663162465',
+    measurementId: 'G-RGJ1F6KQ8H',
+  },
   production: false
 };
 
